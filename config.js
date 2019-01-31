33036e9560fb5acd8c22143fa430ba1bc6a14473
taoxuejiao <taoxuejiao@souche.com>
2019-01-31 15:22:33 +0800

    feat: delete not use package

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const path = require('path')
-const alias = require('rollup-plugin-alias')
-const replace = require('rollup-plugin-replace')
 const flow = require('rollup-plugin-flow-no-whitespace')
+const alias = require('rollup-plugin-alias')
+const cleanup = require('rollup-plugin-cleanup')
+
+const removeComments = false;
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/platforms/compiler.js'),
@@ -15,6 +17,7 @@ module.exports = {
       core: path.resolve(__dirname, 'src/core'),
       shared: path.resolve(__dirname, 'src/shared'),
       web: path.resolve(__dirname, 'src/platforms')
-    })
+    }),
+    removeComments && cleanup()
   ]
 }
